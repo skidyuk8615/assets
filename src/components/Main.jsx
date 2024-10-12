0a670fb0d556e5a19367015b1857c22a56f4a814
skidyuk8615 <chyu23072@g.nihon-u.ac.jp>
2024-10-13 04:44:42 +0900

    Main.jsxの編集

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import WelcomeText from "./WelcomeText"
+import ImageSidebar from "./ImageSidebar"
+
+
+export default function Main() {
+    return (
+        <main>
+            <WelcomeText />
+            <ImageSidebar />
+        </main>
+    );
+}
