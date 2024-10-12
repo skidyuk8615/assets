d8b09d8ca2e8581cc40f9ec7d975519c3400e43d
skidyuk8615 <chyu23072@g.nihon-u.ac.jp>
2024-10-13 03:32:26 +0900

    Header.jsxの編集

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Birdwatching</h1>
+            <div className="header img">
+                <img src="dove.png" alt="a simple dove logo" />
+            </div>
+        </header>
+    );
+}
