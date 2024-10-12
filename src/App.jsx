330a2dc3a43273af9ec7c5131589251ea7431754
skidyuk8615 <chyu23072@g.nihon-u.ac.jp>
2024-10-13 03:28:12 +0900

    main.jsx,App.jsxの編集

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import Nav from "./components/Nav"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Nav />
+            <Main />
+            <Footer />
+        </div>
+    );
+}
