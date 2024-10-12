877e667a35db3aad8d3dfae0e4ef5752930e9cd2
skidyuk8615 <chyu23072@g.nihon-u.ac.jp>
2024-10-13 03:51:54 +0900

    Nav.jsxの編集

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+export default function Nav() {
+    return (
+        <ul>
+            <li><span>Home</span></li>
+            <li><a href="#">Get started</a></li>
+            <li><a href="#">Photos</a></li>
+            <li><a href="#">Gear</a></li>
+            <li><a href="#">Forum</a></li>
+        </ul>
+    )
+}
